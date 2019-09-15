2d560bbf7084c2ec7674532252af7370e260ee28
= <=>
2019-09-15 11:43:30 +0200

    Refactored: Decoupled server implementation from RMI.

--- server/src/main/java/dk/via/cars/RemoteCarBase.java
+++ server/src/main/java/dk/via/cars/RemoteCarBase.java
@@ -1,53 +1,33 @@
 package dk.via.cars;
 
 import java.rmi.RemoteException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class RemoteCarBase implements CarBase {
-	private Map<String, RemoteCar> cars = new HashMap<>();
-	private CarDAO dao;
+	private final CarBaseImplementation implementation;
 
 	public RemoteCarBase(CarDAO dao) {
-		this.dao = dao;
+		implementation = new CarBaseImplementation(dao);
 	}
 	
 	@Override
 	public Car registerCar(String licenseNumber, String model, int year, Money price) throws RemoteException {
-		CarDTO carDTO = dao.create(licenseNumber, model, year, price);
-		RemoteCar car = new RemoteCar(carDTO);
-		cars.put(licenseNumber, car);
-		return car;
+		return implementation.registerCar(licenseNumber, model, year, price);
 	}
 	
 
 	@Override
 	public Car getCar(String licenseNumber) throws RemoteException {
-		if (!cars.containsKey(licenseNumber)) {
-			cars.put(licenseNumber, new RemoteCar(dao.read(licenseNumber)));
-		}
-		return cars.get(licenseNumber);
+		return implementation.getCar(licenseNumber);
 	}
 
 	@Override
 	public List<Car> getAllCars() throws RemoteException {
-		Collection<CarDTO> allCars = dao.readAll();
-		LinkedList<Car> list = new LinkedList<Car>();
-		for(CarDTO car: allCars) {
-			if (!cars.containsKey(car.getLicenseNumber())) {
-				cars.put(car.getLicenseNumber(), new RemoteCar(car));
-			}
-			list.add(cars.get(car.getLicenseNumber()));
-		}
-		return list;
+		return implementation.getAllCars();
 	}
 
 	@Override
 	public void removeCar(Car car) throws RemoteException {
-		dao.delete(new CarDTO(car));
-		cars.remove(car.getLicenseNumber());
+		implementation.removeCar(car);
 	}
 }
