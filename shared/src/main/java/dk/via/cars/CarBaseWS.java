a4392b7b9ca3ff2d4c8d33ca02aed7180f7b716f
OIH <OIH@VIA.DK>
2019-09-30 10:13:27 +0200

    Added meaningful names to generated WSDL

--- shared/src/main/java/dk/via/cars/CarBaseWS.java
+++ shared/src/main/java/dk/via/cars/CarBaseWS.java
@@ -1,6 +1,7 @@
 package dk.via.cars;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import java.util.List;
@@ -8,9 +9,13 @@ import java.util.List;
 @WebService
 @SOAPBinding(style=SOAPBinding.Style.DOCUMENT, use=SOAPBinding.Use.LITERAL)
 public interface CarBaseWS {
-	@WebMethod CarDTO registerCar(String licenseNumber, String model, int year, Money price);
-	@WebMethod void unregisterCar(CarDTO car);
-	@WebMethod CarDTO setPrice(CarDTO car, Money price);
-	@WebMethod CarDTO getCar(String licenseNumber);
+	@WebMethod CarDTO registerCar(@WebParam(name = "licenseNumber") String licenseNumber,
+								  @WebParam(name = "model") String model,
+								  @WebParam(name = "year") int year,
+								  @WebParam(name = "price") Money price);
+	@WebMethod void unregisterCar(@WebParam(name = "car") CarDTO car);
+	@WebMethod CarDTO setPrice(@WebParam(name = "car") CarDTO car,
+							   @WebParam(name = "price") Money price);
+	@WebMethod CarDTO getCar(@WebParam(name = "licenseNumber") String licenseNumber);
 	@WebMethod List<CarDTO> getAllCars();
 }
